7a39ddb2f8f29bd5274e0976ce1e2c3c31c8600b
1000ship <cjstjdgur1234@gmail.com>
2020-01-08 03:00:05 +0900

    Added new page

--- SimpleTimer/ContentView.swift
+++ SimpleTimer/ContentView.swift
@@ -9,32 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var cnt = 0
-    @State var sec = 0
+    
+    @State var cnt : Int = 0
+    @State var sec : Int = 0
+    
     var body: some View {
         NavigationView{
-            Group{
-                VStack(){
-                    Text("You touched me \(cnt) time\(cnt==1 ? "" : "s")")
-                    Button("Touch me!"){
-                        self.cnt += 1
-                    }.padding(Edge.Set.bottom, 40)
-                    Stepper(value: $sec, in: 0...59,
-                        label: {
-                            Text("\(sec) seconds")
+            ZStack(){
+                LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+                Group{
+                    VStack(){
+                        Text("You touched me \(cnt) time\(cnt==1 ? "" : "s")")
+                        Button(action: {self.cnt += 1}){
+                            Text("Touch me!")
+                        }.padding(.bottom, 40)
+                        Stepper(value: $sec, in: 0...59,
+                            label: {
+                                Text("\(sec) seconds")
+                            }
+                        )
+                        NavigationLink(destination: TimerView(sec: sec)) {
+                            Text("Go On")
                         }
-                    )
-                    Spacer()
+                        Image("apple").resizable().scaleEffect(0.3, anchor: .top)
+                        Spacer()
+                    }
                 }
+                .padding(20)
             }
-            .navigationBarTitle( "Touching & Timer" )
-            .padding(20)
-        }
+        }.navigationBarTitle( "Touching & Timer" )
+    }
+}
+
+struct TimerView: View {
+    let sec : Int
+    var body: some View {
+        Text("HELLO \(sec)")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+//        TimerView(sec:5)
     }
 }
