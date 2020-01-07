eda4943c8663f1982d46e63b4695679eaf1b3859
1000ship <cjstjdgur1234@gmail.com>
2020-01-08 02:15:01 +0900

    Added timer stepper

--- SimpleTimer/ContentView.swift
+++ SimpleTimer/ContentView.swift
@@ -10,14 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State var cnt = 0
+    @State var sec = 0
     var body: some View {
         NavigationView{
-            Form{
-                Text("You touched me \(cnt) time\(cnt==1 ? "" : "s")")
-                Button("Touch me!"){
-                    self.cnt += 1
+            Group{
+                VStack(){
+                    Text("You touched me \(cnt) time\(cnt==1 ? "" : "s")")
+                    Button("Touch me!"){
+                        self.cnt += 1
+                    }.padding(Edge.Set.bottom, 40)
+                    Stepper(value: $sec, in: 0...59,
+                        label: {
+                            Text("\(sec) seconds")
+                        }
+                    )
+                    Spacer()
                 }
-            }.navigationBarTitle( "Touching" )
+            }
+            .navigationBarTitle( "Touching & Timer" )
+            .padding(20)
         }
     }
 }
